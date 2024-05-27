1e937f61f48f07439d63e903cab85e7ffd449c13
ychohan77 <ychohan77@gmail.com>
2024-05-27 20:53:51 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+"use strict";
+let grosry = ['Atta', 'Chawal', 'chini', 'pati'];
+console.log(grosry.length);
+//if pickup value of chawal with index
+console.log(grosry[1]);
+// array adding and removeing methods
+grosry.push('Dal');
+grosry.pop();
+console.log(grosry);
---
+++ app.ts
@@ -0,0 +1,11 @@
+let grosry: string[] = ['Atta', 'Chawal', 'chini', 'pati']
+console.log(grosry.length);
+//if pickup value of chawal with index
+console.log(grosry[1])
+// array adding and removeing methods
+grosry.push('Dal')
+grosry.pop()
+console.log(grosry)
+
+
+
